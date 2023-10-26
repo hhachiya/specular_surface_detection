57e2a3d39f09b7fbde9d81b8eb79ca5a3204c01d
yossi-yuto <yoshimura.yuto@g.wakayama-u.jp>
2023-10-26 23:39:25 +0900

    updata new files

--- pipeline_spherical.sh
+++ pipeline_spherical.sh
@@ -4,9 +4,9 @@ DEVICE_NUM=$1
 date=$2
 write_dir=${date}
 for iter in {1,2,3}; do
-    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -mode rccl -write_dir ${write_dir}/ex_${iter}  --train
-    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -mode ssf -write_dir ${write_dir}/ex_${iter}  --train 
-    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -mode sh -write_dir ${write_dir}/ex_${iter}  --train
-    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -mode refine -write_dir ${write_dir}/ex_${iter}  --train --eval
-    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -mode pmd -write_dir ${write_dir}/ex_${iter}  --train --eval
+    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -write_dir ${write_dir}/ex_${iter} -mode rccl --train
+    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -write_dir ${write_dir}/ex_${iter} -mode ssf --train 
+    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -write_dir ${write_dir}/ex_${iter} -mode sh --train
+    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -write_dir ${write_dir}/ex_${iter} -mode refine  --train --eval
+    CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/spherical_mirror_dataset -write_dir ${write_dir}/ex_${iter} -mode pmd --train --eval
 done
